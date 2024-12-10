8545a787fee61c7dab38f59555768d120856d6ae
Yane <yane.yanev@pytek.bg>
2024-12-10 21:46:11 +0200

    Create user model

--- massageProject/accounts/admin.py
+++ massageProject/accounts/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+UserModel = get_user_model()
+
+@admin.register(UserModel)
+class AppUserAdmin(UserAdmin):
+    list_display = ('phone_number', 'email')
+
+    fieldsets = (
+        (None, {"fields": ("phone_number", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("phone_number", "first_name", "last_name", "email")
+    ordering = ("id",)
+
+
--- massageProject/accounts/models.py
+++ massageProject/accounts/models.py
@@ -1,6 +1,76 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
+from django.utils import timezone
+from django.core.mail import send_mail
 
-# Create your models here.
-class CustomUser(AbstractUser):
-    pass
+from massageProject.accounts.managers import AppUserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+        help_text='Enter a valid email address'
+    )
+
+    phone_number = models.CharField(
+        max_length=15,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^(\+359|0)?8[789]\d{7}$',
+                message='Please correct valid phone number'
+            )
+        ],
+        help_text='089999999 or +359899999999'
+    )
+
+    is_staff = models.BooleanField(
+        ("staff status"),
+        default=False,
+        help_text=("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        ("active"),
+        default=True,
+        help_text=(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+
+    count_messages = models.IntegerField(default=0)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = AppUserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ["email"]
+
+    def __str__(self):
+        return self.get_full_name()
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
